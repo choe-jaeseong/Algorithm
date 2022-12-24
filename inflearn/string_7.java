6b6c6949db40a6fa9640a276c551086540fa8df1
jarryChoe <jaisung0410@gmail.com>
2022-12-25 00:05:42 +0900

    solve 10 problems

---
+++ inflearn/string_7.java
@@ -0,0 +1,25 @@
+// 7. 회문 문자열
+import java.util.*;
+
+class Main{
+    
+    public String solution(String str){
+        int i=0, j=str.length()-1;
+				str = str.toUpperCase();
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)) return "NO";
+            i++;
+            j--;
+        }
+        return "YES";
+    }
+    
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
+        
+        return ;
+    }
+}
