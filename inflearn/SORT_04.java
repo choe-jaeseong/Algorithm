b757ce865a30248396310563ecacc5629915de01
jarryChoe <jaisung0410@gmail.com>
2023-01-10 22:45:31 +0900

    swea

--- inflearn/SORT_04.java
+++ inflearn/SORT_04.java
@@ -1,9 +1,22 @@
 // 4. Least Recently Used
+// import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SORT_04 {
 
-    public int[] solution(int s, int n, int[] arr){
+    // ArrayList 로 구현해보기.
+    // public ArrayList<Integer> solution3(int s, int n, int[] arr){
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     for(int i=0; i<n; i++){
+    //         for(int j=0; j<n; j++){
+    //             if(list.isEmpty())
+    //         }
+    //     }
+    //     return list;
+    // }
+
+    //나의 답
+    public int[] solution1(int s, int n, int[] arr){
         int[] cache = new int[s];
         for(int i=0; i<n; i++){                 //arr
             int j=0;
@@ -18,6 +31,28 @@ public class SORT_04 {
         
         return cache;
     }
+
+    //강의 답
+    public int[] solution2(int s, int n, int[] arr){
+        int[] cache = new int[s];
+        for(int x: arr){
+            int pos=-1;                                     //pos 사용
+            for(int i=0; i<s; i++) if(x==cache[i]) pos = i;
+            if(pos==-1){
+                for(int i=s-1; i>=1; i--){
+                    cache[i]=cache[i-1];
+                }
+                cache[0]=x;
+            }
+            else{
+                for(int i=pos; i>=1; i--){
+                    cache[i]=cache[i-1];
+                }
+                cache[0]=x;
+            }
+        }
+        return cache;
+    }
     
     public static void main(String[] args) {
         SORT_04 T = new SORT_04();
@@ -28,7 +63,7 @@ public class SORT_04 {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        for(int x: T.solution(s, n, arr)){
+        for(int x: T.solution1(s, n, arr)){
             System.out.print(x + " ");
         }
         sc.close();
---
+++ inflearn/SORT_06.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SORT_06 {
+    
+    public static void main(String[] args) {
+        SORT_06 T = new SORT_06();
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
