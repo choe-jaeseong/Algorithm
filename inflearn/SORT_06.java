c88deb8ffb1dad1a057025ba4ff8b1d1cd0cb978
jarryChoe <jaisung0410@gmail.com>
2023-01-13 01:09:06 +0900

    solve sort_07

--- inflearn/SORT_06.java
+++ inflearn/SORT_06.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SORT_06 {
+
+    //강의 답
+    public ArrayList<Integer> solution2(int n, int[] arr){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+        for(int i=0; i<n; i++){
+            if(arr[i]!=tmp[i]) ans.add(i+1);
+        }
+        return ans;
+    }
     
-    public void solution(int n, int[] arr){
+    //나의 답 //오답
+    public void solution1(int n, int[] arr){
         int chulsoo=0, jjak=0, cnt = 0;
         for(int i=0; i<n-1; i++){
             if(arr[i]>arr[i+1]) {
@@ -27,7 +41,8 @@ public class SORT_06 {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        T.solution(n, arr);
+        // T.solution1(n, arr);
+        for(int x: T.solution2(n, arr)) System.out.print(x + " ");
         sc.close();
     }
 }
--- inflearn/SORT_07.java
+++ inflearn/SORT_07.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SORT_07 {
+    public int[][] solution(int n, int[][] arr){
+        Arrays.sort(arr, (o1,o2)->{
+            return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1];
+        });
+        return arr;
+    }
     
     public static void main(String[] args) {
-        
+        SORT_07 T = new SORT_07();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+        for(int i=0; i<n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        for(int[] x: T.solution(n, arr)){
+            System.out.println(x[0]+" "+x[1]);
+        }
+        sc.close();
     }
 }
