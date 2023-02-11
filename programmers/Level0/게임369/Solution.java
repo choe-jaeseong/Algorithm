363026d08d295ac3ecd22831e258e8b52f1e10ea
jarryChoe <jaisung0410@gmail.com>
2023-02-11 23:42:37 +0900

    solve 0211

---
+++ programmers/Level0/게임369/Solution.java
@@ -0,0 +1,13 @@
+package Level0.게임369;
+
+public class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order>0){
+            int a = order%10;
+            if(a==3 || a==6 || a==9) answer++;
+            order /= 10;
+        }
+        return answer;
+    }
+}
