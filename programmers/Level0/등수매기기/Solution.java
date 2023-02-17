45398c3c2efa552a21e8a037591a38b1925201e9
jarryChoe <jaisung0410@gmail.com>
2023-02-17 23:52:02 +0900

    solve 0217

---
+++ programmers/Level0/등수매기기/Solution.java
@@ -0,0 +1,11 @@
+package Level0.등수매기기;
+
+public class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = {};
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution T = new Solution();
+    }
+}
---
+++ programmers/Level0/종이자르기/Solution.java
@@ -0,0 +1,9 @@
+package Level0.종이자르기;
+
+public class Solution {
+    public int solution(int M, int N) {
+        return M-1 + (N-1) * M;
+    }
+}
+
+
