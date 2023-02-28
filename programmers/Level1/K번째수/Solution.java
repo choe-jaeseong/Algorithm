c2c4e5efc53699481bedc5e208c7af2fb20edd40
jarryChoe <jaisung0410@gmail.com>
2023-02-28 23:05:07 +0900

    solve 0228

---
+++ programmers/Level1/K번째수/Solution.java
@@ -0,0 +1,96 @@
+package Level1.K번째수;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++){
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i]=list.get(commands[i][2]-1);
+        }
+        return answer;
+    }
+
+    //다른 풀이1
+    public int[] solution1(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+    //다른 풀이2
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int a = 0;
+        for(int[] info : commands){
+            int i = info[0];
+            int j = info[1];
+            int k = info[2];
+
+            int[] buf = Arrays.copyOfRange(array,i-1,j);
+            Arrays.sort(buf);
+            answer[a] = buf[k-1];
+            a++;
+        }
+
+        return answer;
+    }
+    //다른 풀이3  (퀵정렬)
+    public int[] solution3(int[] array, int[][] commands) {
+        int n = 0;
+        int[] ret = new int[commands.length];
+
+        while(n < commands.length){
+            int m = commands[n][1] - commands[n][0] + 1;
+
+            if(m == 1){
+                ret[n] = array[commands[n++][0]-1];
+                continue;
+            }
+
+            int[] a = new int[m];
+            int j = 0;
+            for(int i = commands[n][0]-1; i < commands[n][1]; i++)
+                a[j++] = array[i];
+
+            sort(a, 0, m-1);
+
+            ret[n] = a[commands[n++][2]-1];
+        }
+
+        return ret;
+    }
+
+    void sort(int[] a, int left, int right){
+        int pl = left;
+        int pr = right;
+        int x = a[(pl+pr)/2];
+
+        do{
+            while(a[pl] < x) pl++;
+            while(a[pr] > x) pr--;
+            if(pl <= pr){
+                int temp = a[pl];
+                a[pl] = a[pr];
+                a[pr] = temp;
+                pl++;
+                pr--;
+            }
+        }while(pl <= pr);
+
+        if(left < pr) sort(a, left, pr);
+        if(right > pl) sort(a, pl, right);
+    }
+}
---
+++ programmers/Level1/햄버거만들기/Solution.java
@@ -0,0 +1,63 @@
+package Level1.햄버거만들기;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution {
+    //첫 번째 풀이 - 시간 초과!
+    public int solution(int[] ingredient) {
+        int ans = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<ingredient.length; i++){
+            list.add(ingredient[i]);
+        }
+        boolean flag = true;
+        while(list.size()>0){
+            if(list.size()<4) return ans;
+            if(flag==false) return ans;
+            for(int i=0; i<list.size()-3; i++){
+                if(list.get(i)==1 && list.get(i+1)==2 && list.get(i+2)==3 && list.get(i+3)==1){
+                    ans++;
+                    list.remove(i+3);
+                    list.remove(i+2);
+                    list.remove(i+1);
+                    list.remove(i);
+                    flag = true;
+                    break;
+                } else {
+                    flag = false;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //두 번째 풀이
+    public int solution2(int[] ingredient) {
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int in : ingredient){
+            stack.push(in);
+
+            //스택 사이즈 4이상이 되면 조건 처리 시작
+            if(stack.size() >= 4){
+                //스택 안에 1231이 연이어 들어있을 경우
+                if(stack.get(stack.size()-4)==1
+                && stack.get(stack.size()-3)==2
+                && stack.get(stack.size()-2)==3
+                && stack.get(stack.size()-1)==1) {
+
+                    ans++;
+                    stack.pop(); stack.pop(); stack.pop(); stack.pop();
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[] arr = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        System.out.println(T.solution2(arr));
+    }
+}
