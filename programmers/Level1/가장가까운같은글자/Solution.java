82edb36978bee17d5a2747d34e7dbe72f168aece
jarryChoe <jaisung0410@gmail.com>
2023-03-06 23:03:55 +0900

    solve 0306

--- programmers/Level1/가장가까운같은글자/Solution.java
+++ programmers/Level1/가장가까운같은글자/Solution.java
@@ -1,5 +1,7 @@
 package Level1.가장가까운같은글자;
 
+import java.util.HashMap;
+
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
@@ -12,12 +14,23 @@ class Solution {
             } else {
                 for(int j=i-1; j>=0; j--){
                     if(s.charAt(j)==s.charAt(i)){
-                        System.out.println(s.charAt(i) + ": "+s.charAt(j) + " | "+ i + ", "+ j);
                         answer[i] = i-j;
                         break;
                     }
                 }
             }
+        }
+        return answer;
+    }
+
+    //다른 풀이1
+    public int[] solution1(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
         }
         return answer;
     }
