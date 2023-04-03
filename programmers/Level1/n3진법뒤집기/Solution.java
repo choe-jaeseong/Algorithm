fcffc3071bf0a9b23f701e6f7d041a3e2409adf6
jarryChoe <jaisung0410@gmail.com>
2023-04-03 22:54:51 +0900

    solve 0403

--- programmers/Level1/n3진법뒤집기/Solution.java
+++ programmers/Level1/n3진법뒤집기/Solution.java
@@ -3,19 +3,38 @@ package Level1.n3진법뒤집기;
 public class Solution {
     public int solution(int n) {
         String arith3_reverse = "";
-        long answer = 0;
         while(n>0){
             arith3_reverse+=n%3;
             n/=3;
         }
-        n = Integer.parseInt(arith3_reverse, 3);
-        System.out.println(n);
-        int i=0;
-        while(n>0){
-            answer += ((long)n%10) * Math.pow(3, i++);
-            n /= 10;
+        return Integer.parseInt(arith3_reverse, 3);
+    }
+    //다른 풀이1
+    public int solution1(int n) {
+        String a = "";
+
+        while(n > 0){
+            a = (n % 3) + a;
+            n /= 3;
         }
-        return (int)answer;
+        a = new StringBuilder(a).reverse().toString();
+
+
+        return Integer.parseInt(a,3);
+    }
+    //다른 풀이2
+    public int solution2(int n) {
+        int answer = 0;
+        String third = Integer.toString(n, 3);
+        StringBuffer sb = new StringBuffer(third);
+        String reversed = sb.reverse().toString();
+
+        int exp = 0;
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            answer += Integer.parseInt(String.valueOf(reversed.charAt(i))) * Math.pow(3, exp);
+            exp++;
+        }
+        return answer;
     }
     public static void main(String[] args) {
         Solution T = new Solution();
---
+++ programmers/Level1/practice.java
@@ -0,0 +1,7 @@
+package Level1;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("10",2));
+    }
+}
