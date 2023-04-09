ced08c3547b17a28a8305ac2b7408ac29ac5772a
jarryChoe <jaisung0410@gmail.com>
2023-04-09 23:53:06 +0900

    solve 04099

---
+++ programmers/Level1/바탕화면정리/Solution.java
@@ -0,0 +1,5 @@
+package Level1.바탕화면정리;
+
+public class Solution {
+    
+}
--- programmers/Level1/신고결과받기/Solution.java
+++ programmers/Level1/신고결과받기/Solution.java
@@ -3,7 +3,10 @@ package Level1.신고결과받기;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
@@ -28,8 +31,6 @@ public class Solution {
             String key1 = key.next();
             if(map.get(key1)<k) map.remove(key1);
         }
-        for(String x : map.keySet()) System.out.println(x + " " + map.get(x));
-        
         //유저별로 신고한 사람 중 신고 명단에 포함된 갯수 count
         for(int i=0; i<report.length; i++){
             if(ch[i]==1) continue;
@@ -37,8 +38,7 @@ public class Solution {
             if(map.containsKey(tmp[1]))
                 ans.put(tmp[0], ans.getOrDefault(tmp[0], 0)+1);
         }
-        for(String x : ans.keySet()) System.out.println(x + " " + ans.get(x));
-        
+        //유저가 받은 메일 수 배열 생성
         int[] answer = new int[id_list.length];
         for(int i=0; i<answer.length; i++){
             if(ans.get(id_list[i])==null) answer[i]=0;
@@ -53,5 +53,65 @@ public class Solution {
         for(int x : T.solution(arr, arr1, 2)){
             System.out.println(x+ " ");
         }
+    }
+
+
+    //다른 풀이1
+    public int[] solution1(String[] id_list, String[] report, int k) {
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
+        }
+
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
+    }
+
+    //다른 풀이2
+    public int[] solution2(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        ArrayList<User> users = new ArrayList<>();
+        HashMap<String,Integer> suspendedList = new HashMap<>(); //<이름>
+        HashMap<String,Integer> idIdx = new HashMap<String,Integer>(); // <이름, 해당 이름의 User 클래스 idx>
+        int idx = 0;
+
+        for(String name : id_list) {
+            idIdx.put(name,idx++);
+            users.add(new User(name));
+        }
+        for(String re : report){
+            String[] str = re.split(" ");
+            //suspendedCount.put(str[0], suspendedCount.getOrDefault(str[0],0)+1);
+            users.get( idIdx.get(str[0])).reportList.add(str[1]);
+            users.get( idIdx.get(str[1])).reportedList.add(str[0]);
+        }
+        for(User user : users){
+            if(user.reportedList.size() >= k)
+                suspendedList.put(user.name,1);
+        }
+        for(User user : users){
+            for(String nameReport : user.reportList){
+                if(suspendedList.get(nameReport) != null){
+                    answer[idIdx.get(user.name)]++;
+                }
+
+            }
+        }
+        return answer;
+    }
+}
+class User{
+    String name;
+    HashSet<String> reportList;
+    HashSet<String> reportedList;
+    public User(String name){
+        this.name = name;
+        reportList = new HashSet<>();
+        reportedList = new HashSet<>();
     }
 }
