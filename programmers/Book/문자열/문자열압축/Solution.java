ceff76443111ceb77d54857afcdc73d034d8f62f
jarryChoe <jaisung0410@gmail.com>
2023-05-03 16:16:54 +0900

    solve 0502

--- programmers/Book/문자열/문자열압축/Solution.java
+++ programmers/Book/문자열/문자열압축/Solution.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    
     private List<String> split(String source, int length){
         List<String> tokens = new ArrayList<>();
-        //source를 length만큼 잘라 tokens 리스트에 추가
         for(int startIndex=0; startIndex<source.length(); startIndex+=length){
             int endIndex = startIndex + length;
             if(endIndex > source.length()) endIndex = source.length();
@@ -17,17 +17,33 @@ public class Solution {
     private int compress(String source, int length){
         StringBuilder builder = new StringBuilder();
 
-        for(String token : split(source, length)) {
-            
+        String last = "";
+        int count = 0;
+        for(String token : split(source, length)){
+            //압축 문자열 구성
+            if(token.equals(last)){
+                count++;
+            } else {
+                // 새로운 토큰 정리
+                if(count>1) builder.append(count);
+                builder.append(last);
+                last = token;
+                count = 1;
+            }
         }
-        //압축한 문자열의 길이 반환
-
+        if(count>1) builder.append(count);
+        builder.append(last);
+            
         return builder.length();
     }
     public int solution(String s){
-        int min = Integer.MIN_VALUE;
-        for(int i=1; i<=s.length(); i++){
+        int min = Integer.MAX_VALUE;
+        for(int length=1; length<=s.length(); length++){
             //문자열 압축 후 가장 짧은 길이 선택
+            int compressed = compress(s, length);
+            if(compressed < min){
+                min = compressed;
+            }
         }
         return min;
     }
