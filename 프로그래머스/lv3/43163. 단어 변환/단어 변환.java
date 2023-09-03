8e3d34e66e61975e9d9e8184381ae79a0cb2f1c3
Jarry Choe <96658361+choe-jaeseong@users.noreply.github.com>
2023-09-03 21:30:51 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,7 +1,6 @@
 class Solution {
     
     private static boolean[] visited;
-    private static boolean isExist = false;
     private static int answer = Integer.MAX_VALUE;
     public static int solution(String begin, String target, String[] words) {
         visited = new boolean[words.length];
@@ -9,9 +8,8 @@ class Solution {
         return answer > words.length ? 0 : answer;
     }
     private static void DFS(int L, String now, String target, String[] words){
-        if(L==words.length) return;
+        if(L==words.length+1) return;
         if(now.equals(target)) {
-            isExist = true;
             answer = answer > L ? L : answer;
             return;
         }
@@ -29,16 +27,13 @@ class Solution {
         for(int i=0; i<now.length(); i++){
             if(now.charAt(i) == words[index].charAt(i)) count++;
         }
-        System.out.println(count);
-        boolean result = count==now.length()-1 ? true : false;
-        System.out.println(result);
-        return result;
+        return count==now.length()-1 ? true : false;
     }
 }
 /*
 1. DFS 실행
     - 현재 문자와 알파벳이 하나만 다른 문자를 찾는다.
-    - 그 문자로 이동한다. 방문 체크
-    - 현재 문자가 target 이면 flag = true, 깊이 저장.
-    - 깊이가 현재 주어진 단어 갯수이고 target 이 아니면, 
+    - 그 문자로 이동한다. 방문 체크.
+    - 깊이가 현재 주어진 단어 갯수보다 크면 return.
+    - 현재 문자가 target 이면, 깊이를 answer에 저장.(더 작은 값으로 저장)
 */
