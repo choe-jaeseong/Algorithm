623ee256ffcfc96b3e3d9f6fdab83af21c039456
Jarry Choe <96658361+choe-jaeseong@users.noreply.github.com>
2023-11-13 00:08:03 +0900

    [Silver V] Title: 수들의 합, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        long sum = 0;
+        int count = 0;
+        for(int i=1; ; i++){
+            sum += i;
+            count++;
+            if(sum > N) break;
+        }
+        System.out.println(count - 1);
+    }
+}
