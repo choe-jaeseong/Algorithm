20bbe8f5729535c2804fdb15c363fec169935111
Jae Seong <jaisung0410@gmail.com>
2024-01-03 21:28:31 +0900

    [Gold II] Title: 트리의 지름, Time: 756 ms, Memory: 104132 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -5,7 +5,10 @@ import java.util.*;
 
 public class Main {
 
-    private static Node[] tree;
+    private static List<List<int[]>> tree;
+    private static boolean[] ch;
+    private static int selected;
+    private static int MAX_DISTANCE = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,58 +16,46 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
 
         //트리 생성
-        tree = new Node[N+1];
-        for (int i = 0; i < N+1; i++) tree[i] = new Node(i);
+        tree = new ArrayList<>();
+        for (int i = 0; i < N+1; i++) tree.add(new ArrayList<>());
 
         //트리 간선 형성
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            int target = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
 
             while(st.hasMoreTokens()) {
-                int childNum = Integer.parseInt(st.nextToken());
-                if(childNum == -1) break;
+                int to = Integer.parseInt(st.nextToken());
+                if(to == -1) break;
                 int distance = Integer.parseInt(st.nextToken());
 
-                tree[target].children.put(tree[childNum], distance);
+                tree.get(from).add(new int[]{to, distance});
             }
         }
 
         //1번 노드로부터 가장 먼 노드 X를 찾는다.
-        longestNum = 0;
-        findLongestNodeNum(tree, 1, 0, new boolean[N + 1]);
-        int longestNodeNum = longestNum;
+        ch = new boolean[N+1];
+        findLongestNodeNum(1, 0);
 
         //X로부터 가장 먼 노드를 찾으면서 거리를 계산한다.
-        longestNum = 0;
-        findLongestNodeNum(tree, longestNodeNum, 0, new boolean[N+1]);
+        ch = new boolean[N+1];
+        findLongestNodeNum(selected, 0);
 
         System.out.println(MAX_DISTANCE);
     }
 
-    private static int longestNum = 0;
-    private static int MAX_DISTANCE = 0;
-    private static void findLongestNodeNum(Node[] tree, int start, int distance, boolean[] ch) {
+    private static void findLongestNodeNum(int start, int distance) {
         if(MAX_DISTANCE < distance) {
             MAX_DISTANCE = distance;
-            longestNum = start;
+            selected = start;
         }
 
         ch[start] = true;
 
-        for(Node node : tree[start].children.keySet()) {
-            if(ch[node.data]) continue;
+        for(int[] node : tree.get(start)) {
+            if(ch[node[0]]) continue;
 
-            findLongestNodeNum(tree, node.data, distance + tree[start].children.get(node), ch);
-        }
-    }
-
-    static class Node {
-        int data;
-        Map<Node, Integer> children = new HashMap<>();  // node, distance
-
-        public Node(int data) {
-            this.data = data;
+            findLongestNodeNum(node[0], distance + node[1]);
         }
     }
 }
