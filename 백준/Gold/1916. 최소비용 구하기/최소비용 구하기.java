5ed2ce2d7648d40ad8e9ef6ddf3d291ab79fe781
Jae Seong <jaisung0410@gmail.com>
2024-01-17 20:12:31 +0900

    [Gold V] Title: 최소비용 구하기, Time: 540 ms, Memory: 54232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -5,8 +5,18 @@ import java.util.*;
 
 public class Main {
 
-    static ArrayList<ArrayList<int[]>> routes = new ArrayList<>();
-    static int[] costs;
+    private static class Bus {
+        int dest;
+        int time;
+
+        Bus(int dest, int time) {
+            this.dest = dest;
+            this.time = time;
+        }
+    }
+
+    static ArrayList<Bus>[] routes;
+    static int[] taken;
     static int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +25,18 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<n + 1; i++) routes.add(new ArrayList<>());
-        costs = new int[n + 1];
-        Arrays.fill(costs, INF);
+        routes = new ArrayList[n + 1];
+        for(int i=0; i<n + 1; i++) routes[i] = (new ArrayList<>());
+
+        taken = new int[n + 1];
+        Arrays.fill(taken, INF);
+
         for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            routes.get(from).add(new int[] {to, cost});
+            routes[from].add(new Bus(to, cost));
         }
 
         st = new StringTokenizer(br.readLine());
@@ -32,27 +45,23 @@ public class Main {
 
         Dijkstra(from, to);
 
-        System.out.println(costs[to]);
+        System.out.println(taken[to]);
     }
 
     private static void Dijkstra(int from, int to) {
-        boolean[] visited = new boolean[costs.length];
-        visited[from] = true;
-        costs[from] = 0;
-
-        PriorityQueue<int[]> Q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        for(int[] bus : routes.get(from)) Q.add(bus);
+        PriorityQueue<Bus> Q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.time, o2.time));
+        Q.add(new Bus(from, 0));
+        taken[from] = 0;
 
         while(!Q.isEmpty()) {
-            int[] bus = Q.poll();
-            if(visited[bus[0]] == true) continue;
-            visited[bus[0]] = true;
+            Bus now = Q.poll();
+            if(now.dest == to) break;
 
-            if(costs[bus[0]] > bus[1]) {
-                costs[bus[0]] = bus[1];
-                for(int[] next : routes.get(bus[0]))
-                    if(visited[next[0]] == false)
-                        Q.add(new int[]{next[0], bus[1] + next[1]});
+            for(Bus next : routes[now.dest]) {
+                if(taken[next.dest] > taken[now.dest] + next.time) {
+                    taken[next.dest] = taken[now.dest] + next.time;
+                    Q.add(new Bus(next.dest, taken[next.dest]));
+                }
             }
         }
     }
