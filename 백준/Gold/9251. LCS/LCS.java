760532fc2f1b050a4b72d8408cb83314ecfbf77b
Jae Seong <jaisung0410@gmail.com>
2024-03-09 07:35:44 +0900

    [Gold V] Title: LCS, Time: 108 ms, Memory: 16496 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[][] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        System.out.println(LCS(str1, str2));
+    }
+
+    static int LCS(String str1, String str2) {
+        int n1 = str1.length();
+        int n2 = str2.length();
+
+        dp = new int[n1+1][n2+1];
+
+        for(int i=1; i<n1+1; i++) {
+            for(int j=1; j<n2+1; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
+    }
+}
