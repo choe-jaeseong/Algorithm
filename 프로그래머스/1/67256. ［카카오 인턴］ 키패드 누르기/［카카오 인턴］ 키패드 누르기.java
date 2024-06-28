1131b92ecfefb40610bba5818072bf88fd7d27f1
Jae Seong <jaisung0410@gmail.com>
2024-06-28 09:48:54 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.63 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,85 +1,50 @@
-import java.util.*;
-
 class Solution {
-    private static int[][] phone = {
-        {3, 1}, 
-        {0,0}, {0,1}, {0,2},
-        {1,0}, {1,1}, {1,2},
-        {2,0}, {2,1}, {2,2},
+    //        0부터 9까지 좌표 {y,x}
+    int[][] numpadPos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
     };
-    private static Set<Integer> left = new HashSet<>(List.of(1,4,7));
-    private static Set<Integer> right = new HashSet<>(List.of(3,6,9));
-    
+    //초기 위치
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
     public String solution(int[] numbers, String hand) {
-        StringBuilder sb = new StringBuilder();
-        
-        Hand lh = new Hand(3,0);
-        Hand rh = new Hand(3,2);
-        
-        for(int number : numbers) {
-            if (left.contains(number)) {
-                sb.append("L");
-                lh.handle(number);
-            } else if (right.contains(number)) {
-                sb.append("R");
-                rh.handle(number);
-            } else {
-                // 거리 비교
-                int dist_l = lh.getDistance(number);
-                int dist_r = rh.getDistance(number);
-                if(dist_l < dist_r) {
-                    sb.append("L");
-                    lh.handle(number);
-                } else if(dist_l > dist_r) {
-                    sb.append("R");
-                    rh.handle(number);
-                } else {
-                    if(hand.equals("left")) {
-                        sb.append("L");
-                        lh.handle(number);
-                    } else {
-                        sb.append("R");
-                        rh.handle(number);
-                    }
-                }
-            }
+        this.hand = (hand.equals("right")) ? "R" : "L";
+
+        String answer = "";
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
+
+            if(Umji.equals("L")) {leftPos = numpadPos[num]; continue;}
+            if(Umji.equals("R")) {rightPos = numpadPos[num]; continue;}
         }
-        
-        return sb.toString();
+        return answer;
     }
-    
-    
-    class Hand {
-        int x;
-        int y;
 
-        Hand(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    //num버튼을 누를 때 어디 손을 사용하는가
+    private String pushNumber(int num) {
+        if(num==1 || num==4 || num==7) return "L";
+        if(num==3 || num==6 || num==9) return "R";
 
-        int getDistance(int number) {
-            int[] loc = phone[number];
-            return Math.abs(loc[0] - x) + Math.abs(loc[1] - y);
-        }
+        // 2,5,8,0 일때 어디 손가락이 가까운가
+        if(getDist(leftPos, num) > getDist(rightPos, num)) return "R";
+        if(getDist(leftPos, num) < getDist(rightPos, num)) return "L";
 
-        void handle(int number) {
-            int[] loc = phone[number];
-            x = loc[0];
-            y = loc[1];
-        }
+        //같으면 손잡이
+        return this.hand;
+    }
+
+    //해당 위치와 번호 위치의 거리
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0]-numpadPos[num][0]) + Math.abs(pos[1]-numpadPos[num][1]);
     }
 }
-
-
-/*
-1. 각 번호의 x, y 좌표를 입력한 배열을 만든다.
-2. 왼손 번호(1,4,7), 오른손 번호(3,6,9) set 만들기
-3. 왼손 위치, 오른손 위치 만들기(각 시작 지점)
-4. 판단해서 손 옮기기
-    - 왼손 set 인지 
-    - 오른손 set 인지
-    - 해당 번호 위치 와 왼손 오른손 거리 비교
-        - 가까운 손을 움직이기
-        - 거리가 같다면 손잡이로 움직이기
-*/
