8e3defd196ce60db76366e7081186df81f680f6d
Jae Seong <jaisung0410@gmail.com>
2024-07-03 01:18:51 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,14 +3,18 @@ import java.util.Arrays;
 public class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        
         for(int i=0; i<commands.length; i++){
-            int[] arr = new int[commands[i][1]-commands[i][0]+1];
-            for(int j=commands[i][0]; j<=commands[i][1]; j++){
-                arr[j-commands[i][0]]=array[j-1];
-            }
-            Arrays.sort(arr);
-            answer[i]=arr[commands[i][2]-1];
+            int[] command = commands[i];
+            int from = command[0] - 1;  //0 base
+            int to = command[1];        //0 base exclusive
+            int k = command[2] - 1;     //0 base
+            
+            int[] sub = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(sub);
+            answer[i] = sub[k];
         }
+        
         return answer;
     }
 }
