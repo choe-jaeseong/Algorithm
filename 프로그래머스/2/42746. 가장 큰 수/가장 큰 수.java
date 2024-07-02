634ed43fc04cd2c96a402b33450f502e7c2030dd
Jae Seong <jaisung0410@gmail.com>
2024-07-03 03:03:42 +0900

    [level 2] Title: 가장 큰 수, Time: 1.65 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        String[] numbersToString = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++) {
-            numbersToString[i] = String.valueOf(numbers[i]);
-        }
-        
-        Arrays.sort(numbersToString, (s1, s2) -> {
-            int original = Integer.parseInt(s1 + s2);
-            int reversed = Integer.parseInt(s2 + s1);
-            return reversed - original;
-        });
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<numbersToString.length; i++) {
-            sb.append(numbersToString[i]);
-        }
-        return sb.toString().replaceAll("^0+", "0");
+        String answer = "";
+        return answer;
     }
 }
