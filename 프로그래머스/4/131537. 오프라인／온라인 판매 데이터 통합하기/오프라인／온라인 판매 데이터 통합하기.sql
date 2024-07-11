8eb354c9c6bc1bbd972dc5f5755d239188bcd0a2
Jae Seong <jaisung0410@gmail.com>
2024-07-11 23:18:00 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,27 +1,9 @@
--- 코드를 입력하세요
-# 2022년 3월의 
-# 오프라인/온라인 상품 판매 데이터의 
-# 판매 날짜, 
-# 상품ID, 
-# 유저ID, 
-# 판매량
 
-# SELECT date_format(SALES_DATE, "%Y-%m-%d") SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
-# from ONLINE_SALE
-
-# union all
-
-# SELECT date_format(SALES_DATE, "%Y-%m-%d") SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT
-# from ONLINE_SALE 
-
-# where SALES_DATE between '2022-03-01' and '2022-03-31'
-# order by SALES_DATE, PRODUCT_ID, USER_ID
-
-SELECT date_format(sales_date, '%Y-%m-%d') sales_date, product_id, user_id, sales_amount
-from online_sale
-where date_format(sales_date, '%Y-%m') = '2022-03'
-union all
-select date_format(sales_date, '%Y-%m-%d')sales_date, product_id, NULL as user_id, sales_amount
-from offline_sale 
-where date_format(sales_date, '%Y-%m') = '2022-03'
-order by sales_date, product_id, user_id
+    SELECT DATE_FORMAT(N.SALES_DATE, "%Y-%m-%d") SALES_DATE, N.PRODUCT_ID, N.USER_ID, N.SALES_AMOUNT
+    FROM ONLINE_SALE N
+    WHERE SALES_DATE LIKE "2022-03%"
+UNION ALL
+    SELECT DATE_FORMAT(F.SALES_DATE, "%Y-%m-%d") SALES_DATE, F.PRODUCT_ID, NULL USER_ID, F.SALES_AMOUNT
+    FROM OFFLINE_SALE F
+    WHERE SALES_DATE LIKE "2022-03%"
+ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
