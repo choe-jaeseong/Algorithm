fd317d4f9688d71d4a758c2e61ba5301e5677029
Jae Seong <jaisung0410@gmail.com>
2024-07-12 02:16:23 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -13,20 +13,3 @@ join (select REST_ID, max(favorites) FAVORITES, food_type
         ORDER BY FOOD_TYPE DESC) B
 on A.food_type = B.food_type and A.FAVORITES = B.FAVORITES
 order by A.food_type desc
-
-# SELECT
-# r.FOOD_TYPE,
-# r.REST_ID,
-# r.REST_NAME,
-# r.FAVORITES
-# FROM
-# REST_INFO r JOIN (
-#     SELECT
-#     FOOD_TYPE,
-#     MAX(FAVORITES) FAVORITES
-#     FROM REST_INFO
-#     GROUP BY FOOD_TYPE
-#     ORDER BY FOOD_TYPE DESC
-# ) s
-# ON r.FOOD_TYPE = s.FOOD_TYPE AND r.FAVORITES = s.FAVORITES
-# ORDER BY r.FOOD_TYPE DESC;
