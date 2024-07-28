d47993daa89651af9163acf44f9f58c96d67cfcd
Jae Seong <jaisung0410@gmail.com>
2024-07-28 20:53:07 +0900

    [level 2] Title: 괄호 회전하기, Time: 24.62 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,33 +2,31 @@ import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        String str = s;
+        char[] str = s.toCharArray();
+        
+        int count = 0;
         for(int i=0; i<s.length(); i++) {
-            str = s.substring(i) + s.substring(0, i);
-            if(isCorrect(str)) answer++;
+            if(isCorrect(str, i)) 
+                count++;
         }
-        return answer;
+        return count;
     }
     
-    private static boolean isCorrect(String s) {
+    private static boolean isCorrect(char[] str, int offset) {
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()) {
+        
+        for(int i=0; i<str.length; i++) {
+            char c = str[(offset + i) % str.length];
             switch(c) {
-                case '(', '[', '{' -> stack.add(c);
-                default -> {
+                case '(' -> stack.push(')');
+                case '[' -> stack.push(']');
+                case '{' -> stack.push('}');
+                case ')', ']', '}' -> {
                     if(stack.isEmpty()) return false;
-                    if(c == ')') {
-                        if(stack.pop() != '(') return false;
-                    } else if(c == ']') {
-                        if(stack.pop() != '[') return false;
-                    } else if(c == '}') {
-                        if(stack.pop() != '{') return false;
-                    }
+                    if(stack.pop() != c) return false;
                 }
             }
         }
-        if(!stack.isEmpty()) return false;
-        return true;
+        return stack.isEmpty();
     }
 }
