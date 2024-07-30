298f3f69cadb2faad2eee30ab6ad5cc5fded0555
Jae Seong <jaisung0410@gmail.com>
2024-07-30 14:42:22 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -6,3 +6,10 @@ WHERE ITEM_ID NOT IN (
     WHERE PARENT_ITEM_ID IS NOT NULL
 )
 ORDER BY ITEM_ID DESC;
+
+# SELECT *
+# from item_info info
+# left join item_tree tree 
+# on info.item_id = tree.parent_item_id
+# where parent_item_id is null
+# ORDER BY ITEM_ID DESC;
