2d998acc8af9554e180c29f3f9d996728112dd97
Jae Seong <jaisung0410@gmail.com>
2024-08-01 01:22:28 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,16 +1,10 @@
--- 코드를 입력하세요
-SELECT HISTORY_ID, CAR_ID, 
-    DATE_FORMAT(START_DATE, "%Y-%m-%d") START_DATE, 
-    DATE_FORMAT(END_DATE, "%Y-%m-%d") END_DATE, 
-    case when DATEDIFF(END_DATE+1, START_DATE) >= 30 
-        then "장기 대여" 
-        else "단기 대여" end as RENT_TYPE
+# 대여 시작일이 2022년 9월에 속하는 대여 기록에 대해서 
+# 대여 기간이 30일 이상이면 '장기 대여' 그렇지 않으면 '단기 대여' 로 표시하는 컬럼(컬럼명: RENT_TYPE)을 추가하여 대여기록을 출력
+
+select HISTORY_ID, CAR_ID,
+date_format(START_DATE, '%Y-%m-%d') START_DATE, 
+date_format(END_DATE, '%Y-%m-%d') END_DATE,
+if(datediff(END_DATE, START_DATE) + 1 >= 30, '장기 대여', '단기 대여') RENT_TYPE
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where START_DATE like "2022-09%"
-order by HISTORY_ID DESC
-
-
-
-    # case when DATEDIFF(END_DATE, START_DATE) >= 30 
-    #     then "장기 대여" 
-    #     else "단기 대여" end as RENT_TYPE
+where year(START_DATE) = 2022 and month(START_DATE) = 9
+order by HISTORY_ID desc;
