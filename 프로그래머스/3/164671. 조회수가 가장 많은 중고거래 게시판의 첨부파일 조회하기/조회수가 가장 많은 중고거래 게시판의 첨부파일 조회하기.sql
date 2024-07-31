df086364e01243a4ea4974b65b8e64c019bacd60
Jae Seong <jaisung0410@gmail.com>
2024-08-01 08:51:17 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,12 +1,14 @@
--- 코드를 입력하세요
-# SELECT BOARD_ID, max(VIEWS) from USED_GOODS_BOARD
+with maxview as (
+    select BOARD_ID, VIEWS
+    from USED_GOODS_BOARD
+    order by VIEWS desc
+    limit 1
+)
 
 select concat('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) FILE_PATH
 from USED_GOODS_FILE
-where BOARD_ID = (SELECT BOARD_ID
-                    from USED_GOODS_BOARD 
-                    ORDER BY VIEWS DESC
-                    LIMIT 1)
+where BOARD_ID in (
+    select BOARD_ID 
+    from maxview
+)
 order by FILE_ID desc
-
-# select * from USED_GOODS_FILE
