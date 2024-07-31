f42cdf49bbc869ee7ec2cb926a89d495e55f97ba
Jae Seong <jaisung0410@gmail.com>
2024-08-01 00:30:48 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME from animal_ins i
+join animal_outs o on i.animal_id = o.animal_id
+where i.SEX_UPON_INTAKE like "Intact%" 
+and o.SEX_UPON_OUTCOME not like "Intact%"
+order by i.ANIMAL_ID asc;
