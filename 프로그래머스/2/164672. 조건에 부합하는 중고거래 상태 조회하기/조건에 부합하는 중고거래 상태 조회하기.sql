1339139d04100040d146249d69fca5f96aa9c1e6
Jae Seong <jaisung0410@gmail.com>
2024-08-01 08:35:56 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,10 +1,4 @@
--- 코드를 입력하세요
-SELECT  BOARD_ID, WRITER_ID, TITLE, PRICE, 
-        case STATUS 
-            when 'SALE' then '판매중'
-            when 'RESERVED' then '예약중'
-            when 'DONE' then '거래완료'
-        end STATUS
+select BOARD_ID, WRITER_ID, TITLE, PRICE, if(STATUS = 'SALE', '판매중', if(STATUS = 'RESERVED', '예약중', '거래완료')) STATUS
 from USED_GOODS_BOARD
-where CREATED_DATE like '2022-10-05%'
-order by 1 desc
+where CREATED_DATE = "2022-10-05"
+order by BOARD_ID desc
