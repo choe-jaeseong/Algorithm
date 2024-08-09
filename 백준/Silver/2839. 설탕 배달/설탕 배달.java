82503d6083d943117c77a2416831f334b5c9cb15
Jae Seong <jaisung0410@gmail.com>
2024-08-10 08:51:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 72 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,41 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		if(N==4) {
-			System.out.println(-1);
-			return;
-		}
-		if(N==3 || N==5) {
-			System.out.println(1);
-			return;
-		}
-		
-		int[] dp = new int[N+1];
-		dp[3] = 1;
-		dp[5] = 1;
-		
-		for(int i=6; i<=N; i++) {
-			int a = dp[i-3];
-			int b = dp[i-5];
-			if(a==0 && b==0) {
-				continue;
-			} else if (a==0) {
-				dp[i] = b + 1;
-			} else if (b==0) {
-				dp[i] = a + 1;
-			} else {
-				dp[i] = Math.min(a, b) + 1;
-			}
-		}
 
-		if(dp[N]==0) System.out.println(-1);
-		else System.out.println(dp[N]);
-	}
+    private static int[] dp = new int[5001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[3] = 1;
+        dp[5] = 1;
+
+        for(int i=6; i<=N; i++) {
+            if(dp[i - 3] == Integer.MAX_VALUE && dp[i - 5] == Integer.MAX_VALUE) continue;
+            dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+        }
+
+        System.out.println(dp[N] == Integer.MAX_VALUE ? -1 : dp[N]);
+    }
 }
