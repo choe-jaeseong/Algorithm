1d06a2c4230599c0a3fb7f29817a81eb0b05224d
Jae Seong <jaisung0410@gmail.com>
2024-08-10 09:07:37 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15532 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,38 +4,21 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    private static int[] dp = new int[1000_001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N+1];
-        arr[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
 
-        //DP : Bottom-up
-        for(int i=2; i<=N; i++){
-            arr[i] = DP(i, arr);
+        for(int i=4; i<=N; i++) {
+            int one = dp[i - 1];
+            int two = i % 2 == 0 ? dp[i/2] : Integer.MAX_VALUE;
+            int three = i % 3 == 0 ? dp[i/3] : Integer.MAX_VALUE;
+            dp[i] = Math.min(one, Math.min(two, three)) + 1;
         }
 
-        //결과 출력
-        System.out.println(arr[N]);
-    }
-
-    private static int DP(int i, int[] arr) {
-        if(i%3==0 && i%2==0){
-            return Math.min(Math.min(arr[i/3], arr[i/2]), arr[i-1]) + 1;
-        } else if (i%3==0){
-            return Math.min(arr[i/3], arr[i-1]) + 1;
-        } else if (i%2==0){
-            return Math.min(arr[i/2], arr[i-1]) + 1;
-        } else {
-            return arr[i-1] + 1;
-        }
+        System.out.println(dp[N]);
     }
 }
-
-/*
-DP 접근
-1부터 N까지 접근하여 최소 횟수를 찾는다.
-dp[n+1] = Math.min(방법1(n+1) + 방법2(n+1) + 방법3(n+1))
- */
