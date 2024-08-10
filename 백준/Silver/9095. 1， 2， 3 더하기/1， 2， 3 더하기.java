120170206fd08911150bf9e57b911b61ecea33f3
Jae Seong <jaisung0410@gmail.com>
2024-08-10 11:04:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,27 +4,21 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    private static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
         int T = Integer.parseInt(br.readLine());
         for(int t=0; t<T; t++) {
-            count = 0;
             int n = Integer.parseInt(br.readLine());
-            DFS(0, n);
-            System.out.println(count);
-        }
-    }
-
-    private static void DFS(int sum, int n) {
-        if(sum > n) return;
-        if(sum == n) {
-            count++;
-            return;
-        }
-        for(int i=1; i<=3; i++) {
-            DFS(sum + i, n);
+            System.out.println(dp[n]);
         }
     }
 }
