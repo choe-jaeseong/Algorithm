c5bfdb729a323017454f075cf741ab63d1e13209
Jae Seong <jaisung0410@gmail.com>
2024-08-16 04:30:45 +0900

    [level 2] Title: 호텔 대실, Time: 10.10 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,20 +1,36 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 class Solution {
-    
     public int solution(String[][] book_time) {
-        int[] time = new int[10000];
-        for(String[] book : book_time) {
-            check(time, book);
+
+        int[][] bkt = new int[book_time.length][];
+
+        for (int i = 0; i < book_time.length; i++) {
+            bkt[i] = new int[] { parseTime(book_time[i][0]), parseTime(book_time[i][1]) + 10 };
         }
-        int answer = 0;
-        for(int t : time) answer = Math.max(answer, t);
-        return answer;
+
+        Arrays.sort(bkt, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int ans = 0;
+
+        for (int i = 0; i < bkt.length; i++) {
+            while (!que.isEmpty() && que.peek()[1] <= bkt[i][0]) {
+                que.poll();
+            }
+            que.add(bkt[i]);
+            ans = Math.max(ans, que.size());
+        }
+
+        return ans;
     }
-    private static void check(int[] time, String[] book) {
-        int start = Integer.parseInt(book[0].replace(":", ""));
-        int end = Integer.parseInt(book[1].replace(":", "")) + 10;
-        if(end % 100 > 60) end += 40;
-        for(int i=start; i<end; i++) {
-            time[i]++;
-        }
+
+    protected int parseTime(String time) {
+
+        String[] hhmm = time.split(":");
+        int hour = Integer.parseInt(hhmm[0]), min = Integer.parseInt(hhmm[1]);
+        return hour * 60 + min;
+
     }
 }
