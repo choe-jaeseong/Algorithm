6b23492ee9034c4dc77c9785b7f3514bb02ec38a
Jae Seong <jaisung0410@gmail.com>
2024-08-21 10:22:42 +0900

    [level 3] Title: 부대복귀, Time: 498.80 ms, Memory: 210 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    
+    private static int[] d;
+    private static boolean[] visited;
+    private static List<List<Integer>> r = new ArrayList<>();
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        makingRoads(n, roads);
+        
+        d = new int[n + 1];
+        visited = new boolean[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Dijkstra(roads, destination);
+        
+        int[] answer = new int[sources.length];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = d[sources[i]] == Integer.MAX_VALUE ? -1 : d[sources[i]];
+        }
+        return answer;
+    }
+    private static void makingRoads(int n, int[][] roads) {
+        for(int i=0; i<=n; i++) r.add(new ArrayList<>());
+        for(int[] road : roads) {
+            r.get(road[0]).add(road[1]);
+            r.get(road[1]).add(road[0]);
+        }
+    }
+    private static void Dijkstra(int[][] roads, int start) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((r1, r2) -> r1[1] - r2[1]);
+        pq.add(new int[]{start, 0});
+        while(!pq.isEmpty()) {
+            int[] tmp = pq.poll();
+            int to = tmp[0];
+            int dist = tmp[1];
+            if(visited[to]) continue;
+            visited[to] = true;
+            d[to] = dist;
+            
+            for(int next : r.get(to)) {
+                pq.add(new int[]{next, dist + 1});
+            }
+        }
+    }
+}
+
+/*
+다익스트라 알고리즘
+시작점 = 부대
+*/
