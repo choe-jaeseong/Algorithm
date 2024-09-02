b1ca35184933373dfd28a6b6e3db6f3ec6a643c3
Jae Seong <jaisung0410@gmail.com>
2024-09-02 17:36:53 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] costs = new int[n][3];
+        for(int i=0; i<costs.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            costs[i][0] = Integer.parseInt(st.nextToken());
+            costs[i][1] = Integer.parseInt(st.nextToken());
+            costs[i][2] = Integer.parseInt(st.nextToken());
+
+            if(i == 0) continue;
+
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+
+        int answer = Math.min(costs[n-1][0], Math.min(costs[n-1][1], costs[n-1][2]));
+        System.out.println(answer);
+    }
+}
