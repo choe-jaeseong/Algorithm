1a5088bff556727a523382fec2705c4decf3c9c5
Jae Seong <jaisung0410@gmail.com>
2024-09-04 01:53:09 +0900

    [D4] Title: 팩토리얼과 최대공약수, Time: 743 ms, Memory: 19,444 KB -BaekjoonHub

--- SWEA/D4/7466. 팩토리얼과 최대공약수/팩토리얼과 최대공약수.java
+++ SWEA/D4/7466. 팩토리얼과 최대공약수/팩토리얼과 최대공약수.java
@@ -1,20 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
+
         for(int t = 1; t <= T; t++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int result = getGCD(a, b);
-            System.out.println("#" + t + " " + result);
+            bw.append('#');
+            bw.append(t + " " + result + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 
     private static int getGCD(int a, int b) {
