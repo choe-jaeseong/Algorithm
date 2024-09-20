45daaf789651757a1783ffc7a5c570d780a07449
Jae Seong <jaisung0410@gmail.com>
2024-09-21 02:44:02 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,25 +1,33 @@
-with FRONT AS (
-    SELECT SUM(CODE)
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
+with joined as (
+    select ID, EMAIL, SKILL_CODE, code, name, CATEGORY
+    from DEVELOPERS
+    cross join SKILLCODES
+    where SKILL_CODE & code > 0
+), A as (
+    select "A" GRADE, ID, EMAIL from joined
+    where category = "Front End"
+    and id in (
+        select id from joined
+        where name = "Python"
+    )
+), B as (
+    select "B" GRADE, ID, EMAIL from joined
+    where name = "C#"
+    and id not in (
+        select id from A
+    )
+), C as (
+    select "C" GRADE, ID, EMAIL from joined
+    where category = "Front End"
+    and id not in (
+        select id from A
+        union
+        select id from B
+    )
 )
-
-SELECT
-    CASE 
-        WHEN SKILL_CODE & (SELECT * FROM FRONT) 
-            AND SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'python')
-        THEN 'A'
-        
-        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')
-        THEN 'B'
-        
-        WHEN SKILL_CODE & (SELECT * FROM FRONT)
-        THEN 'C'
-    END AS `GRADE`, 
-    ID,
-    EMAIL
-FROM
-    DEVELOPERS
-HAVING
-    GRADE IS NOT NULL
-ORDER BY GRADE, ID;
+select * from A
+union
+select * from B
+union
+select * from C
+order by GRADE, ID
