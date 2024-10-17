bb6a9fdc892ca57cdd60918c509359cd4d543cc7
Jae Seong <jaisung0410@gmail.com>
2024-10-17 21:28:14 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -3,6 +3,6 @@ from APPOINTMENT a
 join PATIENT p on a.pt_no = p.pt_no
 join DOCTOR d on a.MDDR_ID = d.DR_ID
 where a.APNT_YMD like "2022-04-13%"
-and a.APNT_CNCL_YMD is null
-and a.MCDP_CD = 'CS'
+    and a.APNT_CNCL_YMD is null
+    and a.MCDP_CD = 'CS'
 order by a.APNT_YMD asc
